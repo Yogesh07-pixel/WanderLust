fa02ea8404ca03e58a6acfcec9f981278a9a5962
Yogesh07-pixel <yogeshrawat518@gmail.com>
2024-09-11 23:49:56 +0530

    Testing app with flash meassages when registering new user

--- classroom/server.js
+++ classroom/server.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 const PORT = 5000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(flash());
 
 const sessionOptions = {
   secret: "mysupersecretstring",
   resave: false,
   saveUninitialized: true,
 };
-
 app.use(session(sessionOptions));
 
 app.get("/register", (req, res) => {
   let { name = "anonymous" } = req.query;
   req.session.name = name;
+  req.flash("success", "user registered successfully!");
   res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-  res.send(`Hello , ${req.session.name}`);
+  res.render("page.ejs", { name: req.session.name, msg: req.flash("success") });
 });
 // app.get("/test", (req, res) => {
 //   res.send("Test successfull");
